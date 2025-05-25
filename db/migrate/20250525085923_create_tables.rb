5e5a98a61ec31bddd8eb8fdb38e4b08f9b1da854
ka <ka.kaosf@gmail.com>
2025-05-25 18:00:16 +0900

    Update the migration file to create tables

--- db/migrate/20250525085923_create_tables.rb
+++ db/migrate/20250525085923_create_tables.rb
@@ -1,6 +1,23 @@
 class CreateTables < ActiveRecord::Migration[8.0]
   def change
-    create_table :tables do |t|
+    create_table :sns do |t|
+      t.string :uuid, null: false
+      t.integer :t_us, null: false
+      t.string :title, null: false, default: ""
+      t.string :body, null: false, default: ""
+      t.integer :read, null: false, default: 0
+
+      t.timestamps
+    end
+    add_index :sns, :uuid, unique: true
+    add_index :sns, :t_us
+    add_index :sns, :read
+
+    create_table :sn_subscriptions do |t|
+      t.string :endpoint, null: false, default: ""
+      t.string :p256dh, null: false, default: ""
+      t.string :auth, null: false, default: ""
+
       t.timestamps
     end
   end
